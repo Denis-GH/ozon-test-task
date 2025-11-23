dd86b7585ccd923876c755a092f7df0afd14bc31
Denis Vakulenko <00vakulenko@gmail.com>
2025-11-23 13:36:42 +0500

    feat: add debounce

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import { Progress } from "./progress.js"
+import { debounce } from "./utils.js"
 
 const container = document.querySelector(".progress-container")
 const progress = new Progress(container)
 
-document.getElementById("valueInput").addEventListener("input", (event) => {
+document.getElementById("valueInput").addEventListener("input", debounce((event) => {
   progress.setValue(+event.target.value)
-})
+}))
 
 document.getElementById("animateInput").addEventListener("change", (event) => {
   progress.setAnimated(event.target.checked)
